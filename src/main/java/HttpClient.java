e4e24fd42a78d5dce9aeac31e4f4c54d76717805
Fargekritt <lullinj98@gmail.com>
2022-09-13 12:41:52 +0200

    🟢 Should get 200 response code

--- src/main/java/HttpClient.java
+++ src/main/java/HttpClient.java
@@ -3,6 +3,8 @@ import java.net.Socket;
 
 public class HttpClient {
 
+    private final int statusCode;
+
     public HttpClient(String host, int port, String target) throws IOException {
         Socket socket = new Socket(host, port);
         String request = "GET " + target + " HTTP/1.1\r\n" +
@@ -13,16 +15,23 @@ public class HttpClient {
         socket.getOutputStream().write(request.getBytes());
 
 
+        String line = readLine(socket);
+        statusCode = Integer.parseInt(line.split(" ")[1]);
+        System.out.println(line);
+    }
+
+    private static String readLine(Socket socket) throws IOException {
         int c;
-        while ((c = socket.getInputStream().read()) != -1) {
-            System.out.print((char) c);
+        StringBuilder line = new StringBuilder();
+        while ((c = socket.getInputStream().read()) != '\r') {
+            line.append((char) c);
         }
+        return line.toString();
     }
 
 
-
     public int getStatusCode() {
-        return 0;
+        return statusCode;
     }
 
     public static void main(String[] args) throws IOException {
