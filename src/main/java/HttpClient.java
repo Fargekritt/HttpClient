91452e915292f328b9a083a2d23be377633ab736
Fargekritt <lullinj98@gmail.com>
2022-09-13 12:25:56 +0200

    HTTP main ok

--- src/main/java/HttpClient.java
+++ src/main/java/HttpClient.java
@@ -1,6 +1,21 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class HttpClient {
 
-    public static void main(String[] args) {
-        System.out.println("penis");
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("httpbin.org", 80);
+        String request = "GET /html HTTP/1.1\r\n" +
+                "Connection: close\r\n" +
+                "Host: httpbin.org\n" +
+                "\r\n";
+
+        socket.getOutputStream().write(request.getBytes());
+
+
+        int c;
+        while ((c = socket.getInputStream().read()) != -1) {
+            System.out.print((char) c);
+        }
     }
 }
