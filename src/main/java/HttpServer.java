47f24c3ae14fd7673b360b06623b41e545611fb7
Rikatob <89411878+Rikatob@users.noreply.github.com>
2022-09-22 13:12:53 +0200

    Started HttpServer

---
+++ src/main/java/HttpServer.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class HttpServer {
+
+    private final ServerSocket serverSocket;
+
+    public HttpServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new HttpServer(8080).start();
+    }
+
+    private void start() throws IOException {
+
+        Socket socket = serverSocket.accept();
+
+        socket.getOutputStream().write(("HTTP(1.1 200 OK\r\n" +
+                "Content-length: 12\r\n" +
+                "Content-type: text/plain\r\n" +
+                "\r\n" +
+                "Hello World!").getBytes(StandardCharsets.UTF_8));
+    }
+
+
+}
